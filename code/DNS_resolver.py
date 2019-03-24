d9d43894d56e9e7015055dc2dff4bb4b623fc8d5
Ling Tan <lingt.xyz@gmail.com>
2019-03-24 01:55:34 -0400

    test dns

--- code/DNS_resolver.py
+++ code/DNS_resolver.py
@@ -1,22 +1,14 @@
-from urllib3.util import connection
+import dns.resolver
+from pprint import pprint
 
+my_resolver = dns.resolver.Resolver(configure=False)
+# 8.8.8.8 is Google's public DNS server
+my_resolver.nameservers = ['8.8.8.8']
 
-_orig_create_connection = connection.create_connection
+answer = my_resolver.query('baidu.com')
 
+pprint(vars(answer))
 
-def patched_create_connection(address, *args, **kwargs):
-    """Wrap urllib3's create_connection to resolve the name elsewhere"""
-    # resolve hostname to an ip address; use your own
-    # resolver here, as otherwise the system resolver will be used.
-    host, port = address
-    hostname = dns_resolver(host)
-
-    return _orig_create_connection((hostname, port), *args, **kwargs)
-
-
-def dsn_resolver(host):
-    return ''
-
-
-
-connection.create_connection = patched_create_connection
+for mx in answer.rrset.items:
+    print(mx)
+    
