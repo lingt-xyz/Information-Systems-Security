fe37f862f20b52894e18e2d2828bd930fad549a4
Ling Tan <lingt.xyz@gmail.com>
2019-03-25 17:41:26 -0400

    test dns

--- code/DNS_resolver.py
+++ code/DNS_resolver.py
@@ -1,11 +1,12 @@
 import dns.resolver
 from pprint import pprint
+from urllib3.util import connection
 
 my_resolver = dns.resolver.Resolver(configure=False)
 # 8.8.8.8 is Google's public DNS server
 my_resolver.nameservers = ['8.8.8.8']
 
-answer = my_resolver.query('baidu.com')
+answer = my_resolver.query('baidu.com', 'A')
 
 pprint(vars(answer))
 
@@ -14,10 +15,7 @@ for mx in answer.rrset.items:
     
     
     
-    https://stackoverflow.com/questions/22609385/python-requests-library-define-specific-dns
-    
-
-    from urllib3.util import connection
+#https://stackoverflow.com/questions/22609385/python-requests-library-define-specific-dns
 
 
 _orig_create_connection = connection.create_connection
@@ -28,11 +26,16 @@ def patched_create_connection(address, *args, **kwargs):
     # resolve hostname to an ip address; use your own
     # resolver here, as otherwise the system resolver will be used.
     host, port = address
-    hostname = your_dns_resolver(host)
+    hostname = custom_resolver(host, '8.8.8.8')
 
     return _orig_create_connection((hostname, port), *args, **kwargs)
 
 
 connection.create_connection = patched_create_connection
 
-def myResolver(host,dnssrv): r = dns.resolver.Resolver() r.nameservers = dnssrv answers = r.query(host, 'A') for rdata in answers: return str(rdata)
+def custom_resolver(host,dnssrv): 
+    r = dns.resolver.Resolver() 
+    r.nameservers = dnssrv 
+    answers = r.query(host, 'A') 
+    for rdata in answers: 
+        return str(rdata)
